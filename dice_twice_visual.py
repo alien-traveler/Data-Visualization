4a994641c0e4c993d512560b6411b38958fcde91
alien-traveler <jiwenwillluo@gmail.com>
2020-01-08 12:56:36 -0500

    two dice 100_000 times

--- dice_twice_visual.py
+++ dice_twice_visual.py
@@ -9,7 +9,7 @@ die_2 = Die()
 
 # Make some rolls, and store results in a list.
 results = []
-for roll_num in range(1000):
+for roll_num in range(100000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -26,6 +26,6 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling two D6 dice 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+my_layout = Layout(title='Results of rolling two D6 dice 100000 times', xaxis=x_axis_config, yaxis=y_axis_config)
 offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
 
